2e46d8b267ccfab6183972a40f673a479919abbc
gnus <home>
2019-02-04 01:35:37 +0900

    init spring boot

---
+++ src/main/java/gnus/ds/site/GnusSiteApplication.java
@@ -0,0 +1,14 @@
+package gnus.ds.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnusSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnusSiteApplication.class, args);
+		System.out.println("----check----");
+	}
+
+}
